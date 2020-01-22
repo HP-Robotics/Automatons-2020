463fcec05a71fd3765a7a3d8e7c86bc5f5e62964
HP Robotics Driver Station <scottie@hprobotics.org>
2020-01-21 18:46:00 -0600

    Don't repeatedly call setVelocity()

--- src/main/java/frc/robot/commands/SpinWasherCommand.java
+++ src/main/java/frc/robot/commands/SpinWasherCommand.java
@@ -27,12 +27,12 @@ public class SpinWasherCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_subsystem.setVelocity(Constants.washingMachineSpeed);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.setVelocity(Constants.washingMachineSpeed);
   }
 
   // Called once the command ends or is interrupted.
