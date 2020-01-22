2a9096be01dc349c52fbe8a543261e9b4809743c
HP Robotics Driver Station <scottie@hprobotics.org>
2020-01-21 18:45:26 -0600

    Turn off completely, don't set setpoint

--- src/main/java/frc/robot/subsystems/WashingMachineSubsystem.java
+++ src/main/java/frc/robot/subsystems/WashingMachineSubsystem.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -13,49 +11,55 @@ import frc.robot.Constants;
 
 public class WashingMachineSubsystem extends SubsystemBase {
 
-  private TalonSRX spinnerMotor;
-  private PowerDistributionPanel pdp;
+  private final TalonSRX spinnerMotor;
 
-  public WashingMachineSubsystem() {
-    spinnerMotor = new TalonSRX(Constants.washingMachineMotorId); //TODO - Make sure to correct device ID
-    pdp = new PowerDistributionPanel();
-    
-    spinnerMotor.configFactoryDefault();
-    spinnerMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, Constants.washingMachineLimitedCurrent, Constants.washingMachineMaxCurrent, Constants.washingMachineThresholdTime));
+    public WashingMachineSubsystem() {
+        spinnerMotor = new TalonSRX(Constants.washingMachineMotorId);
 
-	/* Config sensor used for Primary PID [Velocity] */
-    spinnerMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.washingMachineTimeout);
+        spinnerMotor.configFactoryDefault();
+        spinnerMotor.configSupplyCurrentLimit(
+                new SupplyCurrentLimitConfiguration(true, Constants.washingMachineLimitedCurrent,
+                        Constants.washingMachineMaxCurrent, Constants.washingMachineThresholdTime));
 
-    /**
-	 * Phase sensor accordingly. 
-     * Positive Sensor Reading should match Green (blinking) Leds on Talon
-     */
-	spinnerMotor.setSensorPhase(false);
+        /* Config sensor used for Primary PID [Velocity] */
+        spinnerMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,
+                Constants.washingMachineTimeout);
 
-	/* Config the peak and nominal outputs */
-	spinnerMotor.configNominalOutputForward(0, Constants.washingMachineTimeout);
-	spinnerMotor.configNominalOutputReverse(0, Constants.washingMachineTimeout);
-	spinnerMotor.configPeakOutputForward(1, Constants.washingMachineTimeout);
-    spinnerMotor.configPeakOutputReverse(-1, Constants.washingMachineTimeout);
+        /**
+         * Phase sensor accordingly. Positive Sensor Reading should match Green
+         * (blinking) Leds on Talon
+         */
+        spinnerMotor.setSensorPhase(false);
 
+        /* Config the peak and nominal outputs */
+        spinnerMotor.configNominalOutputForward(0, Constants.washingMachineTimeout);
+        spinnerMotor.configNominalOutputReverse(0, Constants.washingMachineTimeout);
+        spinnerMotor.configPeakOutputForward(1, Constants.washingMachineTimeout);
+        spinnerMotor.configPeakOutputReverse(-1, Constants.washingMachineTimeout);
 
-	/* Config the Velocity closed loop gains in slot0 */
-	spinnerMotor.config_kF(0, Constants.washingMachineF, Constants.washingMachineTimeout);
-	spinnerMotor.config_kP(0, Constants.washingMachineP, Constants.washingMachineTimeout);
-	spinnerMotor.config_kI(0, Constants.washingMachineI, Constants.washingMachineTimeout);
-	spinnerMotor.config_kD(0, Constants.washingMachineD, Constants.washingMachineTimeout);
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Current", pdp.getCurrent(Constants.washingMachinePDPChannel));
-    if (pdp.getCurrent(Constants.washingMachinePDPChannel) > 0.0) {
-        System.out.println("Current draw: " + pdp.getCurrent(Constants.washingMachinePDPChannel));
+        /* Config the Velocity closed loop gains in slot0 */
+        spinnerMotor.config_kF(0, Constants.washingMachineF, Constants.washingMachineTimeout);
+        spinnerMotor.config_kP(0, Constants.washingMachineP, Constants.washingMachineTimeout);
+        spinnerMotor.config_kI(0, Constants.washingMachineI, Constants.washingMachineTimeout);
+        spinnerMotor.config_kD(0, Constants.washingMachineD, Constants.washingMachineTimeout);
     }
-  }
 
-  public void setVelocity(double velocity) {
-    spinnerMotor.set(ControlMode.Velocity, velocity);
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        /*
+         * SmartDashboard.putNumber("Current",
+         * pdp.getCurrent(Constants.washingMachinePDPChannel)); if
+         * (pdp.getCurrent(Constants.washingMachinePDPChannel) > 0.0) {
+         * System.out.println("Current draw: " +
+         * pdp.getCurrent(Constants.washingMachinePDPChannel)); }
+         */
+    }
+
+    public void setVelocity(final double velocity) {
+    if (velocity == 0.0)
+        spinnerMotor.set(ControlMode.PercentOutput, 0.0);
+    else
+        spinnerMotor.set(ControlMode.Velocity, velocity);
   }
 }
